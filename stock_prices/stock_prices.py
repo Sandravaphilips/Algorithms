bef9a9156d18cd82b184b7615a18e49b6e4ad70e
Sandrava Philips <lacasandra1@gmail.com>
2020-02-19 12:42:19 +0100

    completed the stock_prices exercise

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,6 +3,21 @@
 import argparse
 
 def find_max_profit(prices):
+  # loop through each price from the start to end-1
+  # save that as the cost price
+  # loop through each price following it until end
+  # subtract the cost price from each selling price
+  # return the maximum value
+  index = 0
+  profit = []
+  
+  while index < len(prices)-1:
+    sales_prices = prices[index+1:]
+    for i in sales_prices:
+      profit.append(i-prices[index])
+    index += 1
+
+  return max(profit)
   pass
 
 
